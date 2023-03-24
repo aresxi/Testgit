3b29464c66776e45922466f27aa161927f2c106b
tuchengxi <943502999@qq.com>
2023-03-24 15:25:53 +0800

    更新11

--- src/main/java/com/chinapost/HelloGit.java
+++ src/main/java/com/chinapost/HelloGit.java
@@ -3,5 +3,6 @@ package com.chinapost;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println(" hello  git ");
+        System.out.println("更新 1111 ");
     }
 }
