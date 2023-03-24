932a6f47924bb1e1cf53adb35e757d39b1ab7fa8
tuchengxi <943502999@qq.com>
2023-03-24 16:47:23 +0800

    分支添加

--- src/main/java/com/chinapost/HelloGit.java
+++ src/main/java/com/chinapost/HelloGit.java
@@ -5,6 +5,6 @@ public class HelloGit {
         System.out.println(" hello  git ");
         System.out.println("更新 1111 ");
         System.out.println("创建分支");
-
+        System.out.println("分支添加");
     }
 }
